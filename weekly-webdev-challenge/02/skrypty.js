ea500219826bb210d393cd391f31100da6eca65c
Agnieszka Kochman <kochman.aga@gmail.com>
2017-09-28 18:44:47 +0200

    Menu active link

--- weekly-webdev-challenge/02/skrypty.js
+++ weekly-webdev-challenge/02/skrypty.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
 	$("#nav ul li a[href^='#']").on('click', function(e) {
 
-	   // prevent default anchor click behavior
 	   e.preventDefault();
 
-	   // store hash
 	   var hash = this.hash;
 
-	   // animate
 	   $('html, body').animate({
 	       scrollTop: $(hash).offset().top
 	     }, 300, function(){
-
-	       // when done, add hash to url
-	       // (default click behaviour)
 	       window.location.hash = hash;
 	     });
 
+	});
+
+
+	$(window).scroll(function() {
+		var scrollPosition = $(document).scrollTop();
+		var windowHeight = $(window).height();
+		var offset = 0.4 * windowHeight;
+
+		$("#nav ul li a").each(function() {
+			var currentLink = $(this);
+			var refElement = $(currentLink.attr("href"));
+
+			if (refElement.position().top - offset <= scrollPosition && refElement.position().top + refElement.height() - offset > scrollPosition) {
+				$("#nav ul li").removeClass("active");
+				currentLink.parent("li").addClass("active");
+			} else {
+				currentLink.parent("li").removeClass("active");
+			}
+		});
 	});
 });
